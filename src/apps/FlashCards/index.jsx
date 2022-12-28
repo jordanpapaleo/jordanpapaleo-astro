e1d3eab2bb8bce04619467dfdb03635c1820ff01
Jordan Papaleo <papaleowebdev@gmail.com>
2022-12-28 14:48:10 -0800

    Saved voting to local storage

--- src/apps/FlashCards/index.jsx
+++ src/apps/FlashCards/index.jsx
@@ -12,39 +12,43 @@ import {
   FaCompress,
   FaChevronRight,
   FaChevronLeft,
+  FaThumbsDown,
+  FaThumbsUp,
 } from 'react-icons/fa'
 import { RxShuffle } from 'react-icons/rx'
+
+const initTags = initQuestions.reduce((allTags, q) => {
+  if (q.tags) {
+    q.tags.forEach((t) => {
+      const plop = allTags.some(({ name }) => name === t)
+      if (!plop) {
+        allTags.push({
+          name: t,
+          value: t,
+        })
+      }
+    })
+  }
+
+  return allTags
+}, [])
+
+const getLsVotes = () => {
+  const ls = localStorage.getItem('flashcard')
+  return ls ? JSON.parse(ls) : {}
+}
+
+const saveLsVotes = (d) => {
+  console.log(d)
+  localStorage.setItem('flashcard', JSON.stringify(d))
+}
 
 const Flashcards = (props) => {
   const [cardIndex, setCardIndex] = React.useState(0)
   const [filters, setFilters] = React.useState([])
   const [questions, setQuestions] = React.useState(initQuestions)
   const [fullScreen, setFullScreen] = React.useState(false)
-  const [tags] = React.useState(
-    initQuestions.reduce((allTags, q) => {
-      if (q.tags) {
-        q.tags.forEach((t) => {
-          const plop = allTags.some(({ name }) => name === t)
-          if (!plop) {
-            allTags.push({
-              name: t,
-              value: t,
-            })
-          }
-        })
-      }
-
-      return allTags
-    }, []),
-  )
-
-  const handleFilter = (checked, value) => {
-    if (checked) {
-      setFilters([...filters, value])
-    } else {
-      setFilters(filters.filter((f) => f !== value))
-    }
-  }
+  const [tags] = React.useState(initTags)
 
   React.useEffect(() => {
     if (!filters.length) {
@@ -59,10 +63,50 @@ const Flashcards = (props) => {
     }
   }, [filters])
 
+  const handleFilter = (checked, value) => {
+    if (checked) {
+      setFilters([...filters, value])
+    } else {
+      setFilters(filters.filter((f) => f !== value))
+    }
+  }
+
   const shuffle = () => {
     const shuffled = [...questions].sort(() => (Math.random() > 0.5 ? 1 : -1))
     setQuestions(shuffled)
     setCardIndex(0)
+  }
+
+  const handleNext = () => {
+    if (cardIndex < questions.length - 1) setCardIndex(cardIndex + 1)
+  }
+
+  const handlePrev = () => {
+    if (cardIndex > 0) setCardIndex(cardIndex - 1)
+  }
+
+  const handleUpVote = () => {
+    const id = questions[cardIndex].id
+    const votes = getLsVotes()
+
+    if (votes[id]) {
+      votes[id] = votes[id] + 1
+    } else {
+      votes[id] = 1
+    }
+
+    saveLsVotes(votes)
+  }
+
+  const handleDownVote = () => {
+    const id = questions[cardIndex].id
+    const votes = getLsVotes()
+
+    if (votes[id]) {
+      votes[id] = votes[id] === 1 ? 0 : votes[id] - 1
+    }
+
+    saveLsVotes(votes)
   }
 
   const fsClass =
@@ -70,7 +114,7 @@ const Flashcards = (props) => {
 
   return (
     <>
-      <div>
+      <div className="mb-2">
         <CheckboxGroup
           label={`Filter: ${filters.join(' | ')}`}
           options={tags}
@@ -81,50 +125,60 @@ const Flashcards = (props) => {
           }}
         />
       </div>
-      <br />
-      Cards: {questions.length}
+
+      <div className="mb-2">Cards: {questions.length}</div>
+
       <div className={clsx('flex gap-2 w-[100%]', fullScreen && fsClass)}>
-        <div className="flex flex-col gap-1">
-          <Button
-            title="Previous Question"
-            onClick={() => {
-              if (cardIndex > 0) setCardIndex(cardIndex - 1)
-            }}
-          >
+        <CardGutter>
+          <Button color="gray" title="Previous Question" onClick={handlePrev}>
             <FaChevronLeft />
           </Button>
+
+          <Button color="crimson" onClick={handleDownVote}>
+            <FaThumbsDown />
+          </Button>
+
           <span className="flex-1" />
-          <Button onClick={() => shuffle()} title="Shuffle Questions">
+
+          <Button color="gray" onClick={shuffle} title="Shuffle Questions">
             <RxShuffle />
           </Button>
-        </div>
+        </CardGutter>
+
         {questions
           .filter((meh, i) => i === cardIndex)
-          .map((question) => {
-            return <Card key={question.id} {...question} />
-          })}
-        <div className="flex flex-col gap-1">
-          <Button
-            title="Next Question"
-            onClick={() => {
-              if (cardIndex < questions.length - 1) setCardIndex(cardIndex + 1)
-            }}
-          >
+          .map((question) => (
+            <Card key={question.id} {...question} />
+          ))}
+
+        <CardGutter>
+          <Button color="gray" title="Next Question" onClick={handleNext}>
             <FaChevronRight />
           </Button>
+
+          <Button color="kellyGreen" title="Up vote" onClick={handleUpVote}>
+            <FaThumbsUp />
+          </Button>
+
           <span className="flex-1" />
+
           <Button
+            color="gray"
             onClick={() => setFullScreen(!fullScreen)}
             title={fullScreen ? 'Compress Cards' : 'Expand Cards'}
           >
             {!fullScreen && <FaExpand />}
             {fullScreen && <FaCompress />}
           </Button>
-        </div>
+        </CardGutter>
       </div>
     </>
   )
 }
+
+const CardGutter = ({ children }) => (
+  <div className="flex flex-col gap-1">{children}</div>
+)
 
 const Card = ({ question, answer, tags = [] }) => {
   const [showAnswer, setShowAnswer] = React.useState(false)
@@ -133,11 +187,12 @@ const Card = ({ question, answer, tags = [] }) => {
     <button
       onClick={() => setShowAnswer(!showAnswer)}
       style={{ borderRadius: '4px', borderWidth: 1 }}
-      className="border-nobel p-2 flex-1"
+      className="border-nobel p-2 flex-1 relative"
     >
       {!showAnswer && <h1 className="text-center">{question}</h1>}
       {showAnswer && <p className="text-left">{answer}</p>}
-      <small className="flex gap-1">
+
+      <small className="flex gap-1 absolute left-1 bottom-1">
         {tags.map((t, i) => (
           <span key={i}>{t}</span>
         ))}
--- src/componentsReact/Button.jsx
+++ src/componentsReact/Button.jsx
@@ -1,3 +1,3 @@
 import { Button as FlowButton } from 'flowbite-react'
-
+// https://flowbite-react.com/buttons
 export default FlowButton
