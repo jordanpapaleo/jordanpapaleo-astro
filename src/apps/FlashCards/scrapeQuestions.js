02c5a57d391a604c6257078ec8768effe12200a9
Jordan Papaleo <papaleowebdev@gmail.com>
2023-01-16 14:21:40 -0800

    Created api page scrapers

---
+++ src/apps/FlashCards/scrapeQuestions.js
@@ -0,0 +1,64 @@
+/* eslint-disable prettier/prettier */
+(() => {
+  const quiz = document.querySelector('.side-wrap');
+  const cards = quiz.querySelectorAll('.card');
+  const chapter = document.querySelector('.page-header .main').textContent;
+  const title = document.querySelector('.page-header .sub').textContent;
+
+  console.log(
+    JSON.stringify(
+      Array.from(cards).map((cardEl) => ({
+      chapter,
+      title,
+      question: cardEl.querySelector('.card-header').textContent,
+      answer: cardEl.querySelector('.card-body').textContent,
+      tags: ['cpt', 'vocab', chapter],
+    })),
+      null,
+      2,
+    ),
+  )
+})()
+
+const vocabWithApi = () => {
+  const quiz = document.querySelector('.side-wrap'); const cards = quiz.querySelectorAll('.card'); const chapter = document.querySelector('.page-header .main').textContent; const title = document.querySelector('.page-header .sub').textContent;
+
+  const vocab = Array.from(cards).map((cardEl) => ({
+      chapter,
+      title,
+      question: cardEl.querySelector('.card-header').textContent,
+      answer: cardEl.querySelector('.card-body').textContent,
+      tags: ['cpt', 'vocab', chapter],
+    }));
+
+  const vocabJSON = JSON.stringify(vocab, null, 2);
+  await fetch('http:localhost:4210/vocab', { method: 'POST',  mode: 'cors', headers: { 'Content-Type': 'application/json' }, body: vocabJSON }).then(() => { console.log('save done') }).catch(() => { console.log('save fail') });
+  navigator.clipboard.writeText(vocabJSON).then(() => {console.log('copy done')}).catch(() => {console.log('copy fail')});
+}
+
+javascript:(() => { const quiz = document.querySelector('.side-wrap'); const cards = quiz.querySelectorAll('.card'); const chapter = document.querySelector('.page-header .main').textContent; const title = document.querySelector('.page-header .sub').textContent; const vocab = Array.from(cards).map((cardEl) => ({ chapter, title, question: cardEl.querySelector('.card-header').textContent, answer: cardEl.querySelector('.card-body').textContent, tags: ['cpt', 'vocab', chapter], })); const vocabJSON = JSON.stringify(vocab, null, 2); fetch('http:localhost:4210/vocab', { method: 'POST',  mode: 'cors', headers: { 'Content-Type': 'application/json' }, body: vocabJSON }).then(() => { console.log('save done') }).catch(() => { console.log('save fail') }); navigator.clipboard.writeText(vocabJSON).then(() => {console.log('copy done')}).catch(() => {console.log('copy fail')});})()
+
+
+const withApi = () => {
+  const quiz = document.querySelector('.quiz-review');
+  const cards = quiz.querySelectorAll('.card');
+  const chapter = document.querySelector('.page-header .main').textContent;
+  const title = document.querySelector('.page-header .sub').textContent;
+  const questions = Array.from(cards).map((cardEl) => {
+    const questionMarkdown = cardEl.querySelector('.card-header markdown');
+    const question = questionMarkdown.textContent || questionMarkdown.innerText || '';
+    const optionsMarkdown = cardEl.querySelectorAll('.card-body markdown');
+    const options = Array.from(optionsMarkdown).map((el) => el.textContent || el.innerText || '');
+    const answerEl = cardEl.querySelector('.text-success');
+    const answer = answerEl.textContent || answerEl.innerText || '';
+    const wrongAnswer = cardEl.querySelector('.text-danger');
+    return { question, options, answer, missed: !!wrongAnswer, tags: ['cpt', 'quiz', chapter], title, chapter };
+  });
+
+  const questionsJSON = JSON.stringify(questions, null, 2);
+  await fetch('http:localhost:4210/quiz', { method: 'POST',  mode: 'cors', headers: { 'Content-Type': 'application/json' }, body: questionsJSON }).then(() => { console.log('save done') }).catch(() => { console.log('save fail') });
+  navigator.clipboard.writeText(questionsJSON).then(() => {console.log('copy done')}).catch(() => {console.log('copy fail')});
+}
+
+
+javascript: (() => { const quiz = document.querySelector('.quiz-review'); const cards = quiz.querySelectorAll('.card'); const chapter = document.querySelector('.page-header .main').textContent; const title = document.querySelector('.page-header .sub').textContent; const questions = Array.from(cards).map((cardEl) => { const questionMarkdown = cardEl.querySelector('.card-header markdown'); const question = questionMarkdown.textContent || questionMarkdown.innerText || ''; const optionsMarkdown = cardEl.querySelectorAll('.card-body markdown'); const options = Array.from(optionsMarkdown).map((el) => el.textContent || el.innerText || ''); const answerEl = cardEl.querySelector('.text-success'); const answer = answerEl.textContent || answerEl.innerText || ''; const wrongAnswer = cardEl.querySelector('.text-danger'); return { question, options, answer, missed: !!wrongAnswer, tags: ['cpt', 'quiz', chapter], title, chapter }; }); const questionsJSON = JSON.stringify(questions, null, 2); fetch('http:localhost:4210/quiz', { method: 'POST', mode: 'cors', headers: { 'Content-Type': 'application/json' }, body: questionsJSON }).then(() => { console.log('save done') }).catch(() => { console.log('save fail') }); navigator.clipboard.writeText(questionsJSON).then(() => {console.log('copy done')}).catch(() => {console.log('copy fail')}); })()
---
+++ src/apps/FlashCards/server.js
@@ -0,0 +1,71 @@
+import fs from 'fs'
+import path from 'path'
+import express from 'express'
+import bodyParser from 'body-parser'
+import { v4 as uuidv4 } from 'uuid'
+import cors from 'cors'
+
+const app = express()
+const port = 4210
+const jsonParser = bodyParser.json()
+
+app.use(cors())
+
+app.get('/', (req, res) => res.send('NASM'))
+
+const updateDb = (req, res, fileName) => {
+  fs.readFile(fileName, 'utf8', (err, jsonString) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send({
+        message: 'Db Read fail',
+        err,
+      })
+    }
+
+    try {
+      const newQuestions = req.body
+      const dbQuestions = JSON.parse(jsonString)
+      const allQuestions = [...dbQuestions]
+
+      newQuestions.forEach(({ question: q1 }, i) => {
+        const isDup = allQuestions.some(({ question: q2 }) => q1 === q2)
+        if (isDup) return
+        allQuestions.push({
+          ...newQuestions[i],
+          id: uuidv4(),
+        })
+      })
+
+      fs.writeFile(fileName, JSON.stringify(allQuestions, null, 2), (err) => {
+        if (err) {
+          console.log(err)
+          res.status(500).send({
+            message: 'Write error',
+            err,
+          })
+        } else {
+          res.status(200).send({
+            message: 'Success',
+          })
+        }
+      })
+    } catch (err) {
+      console.log(err)
+      res.status(500).send({
+        message: 'Parse JSON error',
+        err,
+      })
+    }
+  })
+}
+
+app.post('/vocab', jsonParser, (req, res) => {
+  updateDb(req, res, './db/vocab.json')
+})
+
+app.post('/quiz', jsonParser, (req, res) => {
+  updateDb(req, res, './db/quiz.json')
+})
+
+app.listen(port, () => console.log(`CORS-enabled Listening on port ${port}!`))
