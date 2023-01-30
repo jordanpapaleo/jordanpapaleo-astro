cbfd8a4df53123815da6892e39c02a3d07f5dee3
Jordan Papaleo <papaleowebdev@gmail.com>
2023-01-29 16:33:16 -0800

    fixed favlets

--- src/apps/FlashCards/scrapeQuestions.js
+++ src/apps/FlashCards/scrapeQuestions.js
@@ -2,16 +2,16 @@
 (() => {
   const quiz = document.querySelector('.side-wrap');
   const cards = quiz.querySelectorAll('.card');
-  const chapter = document.querySelector('.page-header .main').textContent;
-  const title = document.querySelector('.page-header .sub').textContent;
+  const chapter = document.querySelector('.page-header .main')?.textContent;
+  const title = document.querySelector('.page-header .sub')?.textContent;
 
   console.log(
     JSON.stringify(
       Array.from(cards).map((cardEl) => ({
       chapter,
       title,
-      question: cardEl.querySelector('.card-header').textContent,
-      answer: cardEl.querySelector('.card-body').textContent,
+      question: cardEl.querySelector('.card-header')?.textContent,
+      answer: cardEl.querySelector('.card-body')?.textContent,
       tags: ['cpt', 'vocab', chapter],
     })),
       null,
@@ -21,13 +21,13 @@
 })()
 
 const vocabWithApi = () => {
-  const quiz = document.querySelector('.side-wrap'); const cards = quiz.querySelectorAll('.card'); const chapter = document.querySelector('.page-header .main').textContent; const title = document.querySelector('.page-header .sub').textContent;
+  const quiz = document.querySelector('.side-wrap'); const cards = quiz.querySelectorAll('.card'); const chapter = document.querySelector('.page-header .main')?.textContent; const title = document.querySelector('.page-header .sub')?.textContent;
 
   const vocab = Array.from(cards).map((cardEl) => ({
       chapter,
       title,
-      question: cardEl.querySelector('.card-header').textContent,
-      answer: cardEl.querySelector('.card-body').textContent,
+      question: cardEl.querySelector('.card-header')?.textContent,
+      answer: cardEl.querySelector('.card-body')?.textContent,
       tags: ['cpt', 'vocab', chapter],
     }));
 
@@ -36,14 +36,14 @@ const vocabWithApi = () => {
   navigator.clipboard.writeText(vocabJSON).then(() => {console.log('copy done')}).catch(() => {console.log('copy fail')});
 }
 
-javascript:(() => { const quiz = document.querySelector('.side-wrap'); const cards = quiz.querySelectorAll('.card'); const chapter = document.querySelector('.page-header .main').textContent; const title = document.querySelector('.page-header .sub').textContent; const vocab = Array.from(cards).map((cardEl) => ({ chapter, title, question: cardEl.querySelector('.card-header').textContent, answer: cardEl.querySelector('.card-body').textContent, tags: ['cpt', 'vocab', chapter], })); const vocabJSON = JSON.stringify(vocab, null, 2); fetch('https://jp-api.vercel.app/api/flashcards/vocab', { method: 'PUT',  mode: 'cors', headers: { 'Content-Type': 'application/json' }, body: vocabJSON }).then(() => { console.log('save done') }).catch(() => { console.log('save fail') }); navigator.clipboard.writeText(vocabJSON).then(() => {console.log('copy done')}).catch(() => {console.log('copy fail')});})()
+javascript:(() => { const quiz = document.querySelector('.side-wrap'); const cards = quiz.querySelectorAll('.card'); const chapter = document.querySelector('.page-header .main')?.textContent; const title = document.querySelector('.page-header .sub')?.textContent; const vocab = Array.from(cards).map((cardEl) => ({ chapter, title, question: cardEl.querySelector('.card-header')?.textContent, answer: cardEl.querySelector('.card-body')?.textContent, tags: ['cpt', 'vocab', chapter], })); const vocabJSON = JSON.stringify(vocab, null, 2); fetch('https://jp-api.vercel.app/api/flashcards/vocab', { method: 'PUT',  mode: 'cors', headers: { 'Content-Type': 'application/json' }, body: vocabJSON }).then(() => { alert('success') }).catch(() => { alert('fail') }); })()
 
 
 const quizWithApi = () => {
   const quiz = document.querySelector('.quiz-review');
   const cards = quiz.querySelectorAll('.card');
-  const chapter = document.querySelector('.page-header .main').textContent;
-  const title = document.querySelector('.page-header .sub').textContent;
+  const chapter = document.querySelector('.page-header .main')?.textContent;
+  const title = document.querySelector('.page-header .sub')?.textContent;
   const questions = Array.from(cards).map((cardEl) => {
     const questionMarkdown = cardEl.querySelector('.card-header markdown');
     const question = questionMarkdown.textContent || questionMarkdown.innerText || '';
@@ -57,8 +57,9 @@ const quizWithApi = () => {
 
   const questionsJSON = JSON.stringify(questions, null, 2);
   fetch('https://jp-api.vercel.app/api/flashcards/quiz', { method: 'PUT',  mode: 'cors', headers: { 'Content-Type': 'application/json' }, body: questionsJSON }).then(() => { console.log('save done') }).catch(() => { console.log('save fail') });
-  navigator.clipboard.writeText(questionsJSON).then(() => {console.log('copy done')}).catch(() => {console.log('copy fail')});
+  // navigator.clipboard.writeText(questionsJSON).then(() => {console.log('copy done')}).catch(() => {console.log('copy fail')});
 }
 
 
-javascript: (() => { const quiz = document.querySelector('.quiz-review'); const cards = quiz.querySelectorAll('.card'); const chapter = document.querySelector('.page-header .main').textContent; const title = document.querySelector('.page-header .sub').textContent; const questions = Array.from(cards).map((cardEl) => { const questionMarkdown = cardEl.querySelector('.card-header markdown'); const question = questionMarkdown.textContent || questionMarkdown.innerText || ''; const optionsMarkdown = cardEl.querySelectorAll('.card-body markdown'); const options = Array.from(optionsMarkdown).map((el) => el.textContent || el.innerText || ''); const answerEl = cardEl.querySelector('.text-success'); const answer = answerEl.textContent || answerEl.innerText || ''; const wrongAnswer = cardEl.querySelector('.text-danger'); return { question, options, answer, missed: !!wrongAnswer, tags: ['cpt', 'quiz', chapter], title, chapter }; }); const questionsJSON = JSON.stringify(questions, null, 2); fetch('https://jp-api.vercel.app/api/flashcards/quiz', { method: 'PUT', mode: 'cors', headers: { 'Content-Type': 'application/json' }, body: questionsJSON }).then(() => { console.log('save done') }).catch(() => { console.log('save fail') }); navigator.clipboard.writeText(questionsJSON).then(() => {console.log('copy done')}).catch(() => {console.log('copy fail')}); })()
+javascript: (() => { const quiz = document.querySelector('.quiz-review'); const cards = quiz.querySelectorAll('.card'); const chapter = document.querySelector('.page-header .main')?.textContent; const title = document.querySelector('.page-header .sub')?.textContent; const questions = Array.from(cards).map((cardEl) => { const questionMarkdown = cardEl.querySelector('.card-header markdown'); const question = questionMarkdown.textContent || questionMarkdown.innerText || ''; const optionsMarkdown = cardEl.querySelectorAll('.card-body markdown'); const options = Array.from(optionsMarkdown).map((el) => el.textContent || el.innerText || ''); const answerEl = cardEl.querySelector('.text-success'); const answer = answerEl.textContent || answerEl.innerText || ''; const wrongAnswer = cardEl.querySelector('.text-danger'); return { question, options, answer, missed: !!wrongAnswer, tags: ['cpt', 'quiz', chapter], title, chapter }; }); const questionsJSON = JSON.stringify(questions, null, 2); fetch('https://jp-api.vercel.app/api/flashcards/quiz', { method: 'PUT', mode: 'cors', headers: { 'Content-Type': 'application/json' }, body: questionsJSON }).then(() => { alert('success') }).catch(() => { alert('fail') }); })()
+
